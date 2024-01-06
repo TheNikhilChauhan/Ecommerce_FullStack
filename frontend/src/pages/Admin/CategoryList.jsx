aeccab3eb7880142531f45e75c3ba2cdc0c574be
Nikhil <ace.nikchauhan@gmail.com>
2024-01-06 12:57:59 +0530

    added category form and category list

--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -1,5 +1,124 @@
+import { useState } from "react";
+import {
+  useCreateCategoryMutation,
+  useDeleteCategoryMutation,
+  useFetchCategoryQuery,
+  useUpdateCategoryMutation,
+} from "../../redux/api/categorySlice";
+import { toast } from "react-toastify";
+import CategoryForm from "../../components/CategoryForm";
+
 const CategoryList = () => {
-  return <div className="text-3xl text-white text-center">CategoryList</div>;
+  const { data: categories } = useFetchCategoryQuery();
+  const [name, setName] = useState("");
+  const [selectCategory, setSelectCategory] = useState(null);
+  const [updateName, setUpdateName] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [createCategory] = useCreateCategoryMutation();
+  const [updateCategory] = useUpdateCategoryMutation();
+  const [deleteCategory] = useDeleteCategoryMutation();
+
+  const handleCreateCategory = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      toast.error("Category name is required.");
+      return;
+    }
+
+    try {
+      const result = await createCategory({ name }).unwrap();
+      if (result.error) {
+        toast.error(result.error);
+      } else {
+        setName("");
+        toast.success(`${result.name} is created.`);
+      }
+    } catch (error) {
+      toast.error("Creating category failed, try again.");
+    }
+  };
+
+  const handleUpdateCategory = async (e) => {
+    e.preventDefault();
+
+    if (!updateName) {
+      toast.error("Category name is required.");
+      return;
+    }
+
+    try {
+      const result = await updateCategory({
+        categoryId: selectCategory._id,
+        updateCategory: {
+          name: updateCategory,
+        },
+      }).unwrap();
+
+      if (result.error) {
+        toast.error(result.error);
+      } else {
+        toast.success(`${result.name} is updated.`);
+        setSelectCategory(null);
+        setUpdateName("");
+        setModalVisible(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteCategory = async () => {
+    try {
+      const result = await deleteCategory(selectCategory._id).unwrap();
+
+      if (result.error) {
+        toast.error(result.error);
+      } else {
+        toast.success(`${result.name} is deleted.`);
+        setSelectCategory(null);
+        setModalVisible(false);
+      }
+    } catch (error) {
+      toast.error("Category delectin failed. Try again.");
+    }
+  };
+
+  return (
+    <div className="ml-[10rem] flex flex-col md:flex-row">
+      {/* AdminMenu */}
+      <div className="md:w-3/4 p-3">
+        <div className="h-12">Manage Categories</div>
+        <CategoryForm
+          value={name}
+          setValue={setName}
+          handleSubmit={handleCreateCategory}
+        />
+        <br />
+        <hr />
+
+        <div className="flex flex-wrap">
+          {categories?.map((category) => (
+            <div key={category._id}>
+              <button
+                className="bg-white border border-pink-500 text-pink-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:outline-none foucs:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
+                onClick={() => {
+                  {
+                    setModalVisible(true);
+                    setSelectedCategory(category);
+                    setUpdateName(category.name);
+                  }
+                }}
+              >
+                {category.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryList;
